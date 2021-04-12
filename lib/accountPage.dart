eaf9e7566f1f8bd266863200784120b9521f1947
준성 <madox9999@gmail.com>
2021-04-12 16:49:55 +0900

    Acconut페이지 UI작성

---
+++ lib/accountPage.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatefulWidget {
+  @override
+  _AccountPageState createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      actions: [IconButton(icon: Icon(Icons.exit_to_app), onPressed: () {})],
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              Stack(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage('https://spnimage.edaily.co.kr/images/Photo/files/NP/S/2018/07/PS18070900090.jpg'),
+                    ),
+                  ),
+                  Container(
+                    width: 80,
+                    height:80,
+                    alignment: Alignment.bottomRight,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(
+                          height: 28,
+                          width: 28,
+                          child: FloatingActionButton(onPressed: null,
+                            backgroundColor: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 25,
+                          width: 25,
+                          child: FloatingActionButton(onPressed: null,
+                            backgroundColor: Colors.blue,
+                            child: Icon(Icons.add),),
+
+                        ),
+                      ],
+                    )
+                  )
+                ],
+              ),
+              Padding(padding: EdgeInsets.all(8)),
+              Text(
+                '이름',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Text('0\n게시물',
+              textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+          Text('0\n팔로워',
+              textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+          Text('0\n팔로잉',
+              textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+        ],
+      ),
+    );
+  }
+}
--- lib/tabPage.dart
+++ lib/tabPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagramclon/accountPage.dart';
 import 'package:instagramclon/homePage.dart';
 
 
