1102d2f020084ebb73d6deb0dfeb48f5abe79550
준성 <madox9999@gmail.com>
2021-04-12 16:54:15 +0900

    갤러리에서 사진불러오기 및 로그인페이지 UI작성

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('login page'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Instagram Clon',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            Padding(padding: EdgeInsets.all(40)),
+            SignInButton(Buttons.Google, onPressed: (){})
+          ],
+        ),
+      ),
     );
   }
 }
